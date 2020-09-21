1968e98e677ed6c2c5c49e7595b127230fd1770c
alexandertbratrich <alexander@bratrich-online.de>
2020-09-21 13:39:23 +0200

    V0.1.912-pre

--- src/main/index.js
+++ src/main/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 import { app, BrowserWindow } from 'electron'
+import { autoUpdater } from 'electron-updater'
 
 /**
  * Set `__static` path to static files in production
@@ -26,6 +27,7 @@ function createWindow () {
   })
 
   mainWindow.loadURL(winURL)
+  // mainWindow.webContents.openDevTools() // debugging
 
   mainWindow.on('closed', () => {
     mainWindow = null
@@ -54,9 +56,6 @@ app.on('activate', () => {
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-electron-builder.html#auto-updating
  */
 
-/*
-import { autoUpdater } from 'electron-updater'
-
 autoUpdater.on('update-downloaded', () => {
   autoUpdater.quitAndInstall()
 })
@@ -64,4 +63,3 @@ autoUpdater.on('update-downloaded', () => {
 app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
- */
